413c7cc8b29a4336f82199119522c1d59a771fcd
Amaro Koberle <amaro@Amaros-MBP.lan>
2021-02-18 22:41:07 +0100

    added some placeholder UI for displaying node content

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
       </div>
 
       <div className="left-panel">
-        <div className="menu-header">
+        {/* <div className="menu-header">
           <h3>Edit Node</h3>
           <button className="exit-button">X</button>
         </div>
@@ -29,6 +29,36 @@ function App() {
           <input type="text" id="title"></input>
           <label for="content">Content</label>
           <input type="text" id="content"></input>
+        </form>*/}
+        <div className="menu-header">
+          <h3>Node Title</h3>
+          <button className="edit-button">Edit</button>
+        </div>
+        <form className="node-display">
+          <h5>Node Title</h5>
+
+          <p>
+            Node Content... Lorem ipsum dolor sit amet, consectetur adipiscing
+            elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+            aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+            laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
+            dolor in reprehenderit in voluptate velit esse cillum dolore eu
+            fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+            proident, sunt in culpa qui officia deserunt mollit anim id est
+            laborum.
+          </p>
+          <h5>In</h5>
+          <ul>
+            <li>Origin Node 1</li>
+            <li>Origin Node 2</li>
+            <li>Origin Node 3</li>
+          </ul>
+          <h5>Out</h5>
+          <ul>
+            <li>Destination Node 1</li>
+            <li>Destination Node 2</li>
+            <li>Destination Node 3</li>
+          </ul>
         </form>
       </div>
       <div>
