d7a2b095b919e1ffa55a8af3f5362777a1ef5f79
Amaro Koberle <amaro@Amaros-MBP.lan>
2021-02-18 21:56:14 +0100

    The graph is now derived from a json file rather than generated randomly

--- src/App.js
+++ src/App.js
@@ -1,38 +1,39 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import "./App.css";
-import { ForceGraph2D, ForceGraph3D, ForceGraphVR, ForceGraphAR } from 'react-force-graph';
+import {
+  ForceGraph2D,
+  ForceGraph3D,
+  ForceGraphVR,
+  ForceGraphAR,
+} from "react-force-graph";
+import data from "./graphData.json";
 
 function App() {
-  const [graph, setGraph] = useState(genRandomTree())
-  const [numNodes, setNumNodes] = useState(300)
-
-  function genRandomTree(N = 300, reverse = false) {
-    return {
-      nodes: [...Array(N).keys()].map(i => ({ id: i })),
-      links: [...Array(N).keys()]
-        .filter(id => id)
-        .map(id => ({
-          [reverse ? 'target' : 'source']: id,
-          [reverse ? 'source' : 'target']: Math.round(Math.random() * (id - 1))
-        }))
-    };
-  }
-
-
-  const randomiseGraph = () => {
-    setGraph(genRandomTree(numNodes))
-  }
-
   return (
     <>
       <div className="button-container">
-        <button className="add-node" onClick={() => randomiseGraph()}>
-          RANDOMISE GRAPH
-        </button>
-        <input type="range" value={numNodes} min={1} max={1000} onInput={(e) => setNumNodes(parseInt(e.target.value))} />
-        <span style={{ color: "red" }}>{numNodes}</span>
+        <button className="add-node">New Node</button>
       </div>
-      <ForceGraph3D graphData={graph} />
+
+      <div className="left-panel">
+        <div className="menu-header">
+          <h3>Edit Node</h3>
+          <button className="exit-button">X</button>
+        </div>
+        <form className="edit-node">
+          <label for="in">In</label>
+          <input type="text" id="in"></input>
+          <label for="out">Out</label>
+          <input type="text" id="out"></input>
+          <label for="title">Title</label>
+          <input type="text" id="title"></input>
+          <label for="content">Content</label>
+          <input type="text" id="content"></input>
+        </form>
+      </div>
+      <div>
+        <ForceGraph3D graphData={data} />
+      </div>
     </>
   );
 }
